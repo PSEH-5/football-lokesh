cd89acf17a6944cc92b3ffc74f52f3b6dbedec23
India Hiring <indiahiring@global.publicisgroupe.net>
2020-02-22 15:43:47 +0530

    Football League - Lokesh

--- src/test/java/com/sapient/assessment/football/AppTest.java
+++ src/test/java/com/sapient/assessment/football/AppTest.java
@@ -1,38 +1,40 @@
 package com.sapient.assessment.football;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.sapient.assessment.pojo.StandingResult;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	@MockBean
+	private FootballLeagueStandingService service;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	/**
+	 * Rigourous Test :-)
+	 */
+	@Test
+	public void getLeaguethenReturnJson() throws Exception {
+		StandingResult r = new StandingResult();
+		r.setLeagueId("149");
+		Mockito.when(service.getFinalStandingPosition("England", "Championship", "Stoke")).thenReturn(r);
+		assertTrue(r != null);
+	}
 }
